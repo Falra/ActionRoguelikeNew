bf52de5f0cfbc909ff085cd9e8d26064f0c57a2c
Valerii Diachenko <leriy.dyachenko@gmail.com>
2025-12-17 08:24:44 +0200

    Added projectile actor and collision preset for projectiles

---
+++ Source/ActionRoguelike/Projectiles/RogueProjectileMagic.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+    SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+    RootComponent = SphereComponent;
+    SphereComponent->SetSphereRadius(16.0f);
+    SphereComponent->SetCollisionProfileName("Projectile");
+    
+    LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComponent"));
+    LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+    
+    ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+    ProjectileMovementComponent->InitialSpeed = 2000.0f;
+    ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+}
