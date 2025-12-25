a6a8bd1693baf08ddcf3eb391050898bbf63963f
Valerii Diachenko <leriy.dyachenko@gmail.com>
2025-12-25 08:26:30 +0200

    Added item chest with open animation

--- Source/ActionRoguelike/World/RogueItemChest.cpp
+++ Source/ActionRoguelike/World/RogueItemChest.cpp
@@ -4,25 +4,35 @@
 #include "RogueItemChest.h"
 
 
-// Sets default values
 ARogueItemChest::ARogueItemChest()
 {
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bStartWithTickEnabled = false;
+
+    BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComponent"));
+    RootComponent = BaseMeshComponent;
+
+    LidMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LidMeshComponent"));
+    LidMeshComponent->SetupAttachment(BaseMeshComponent);
 
 }
 
-// Called when the game starts or when spawned
 void ARogueItemChest::BeginPlay()
 {
     Super::BeginPlay();
-    
+    SetActorTickEnabled(true);
 }
 
-// Called every frame
 void ARogueItemChest::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
+    CurrentAnimationPitch = FMath::FInterpConstantTo(CurrentAnimationPitch, AnimationTargetPitch, DeltaTime, AnimationSpeed);
+    LidMeshComponent->SetRelativeRotation(FRotator(CurrentAnimationPitch, 0.0f, 0.0f));
+
+    if (FMath::IsNearlyEqual(CurrentAnimationPitch, AnimationTargetPitch))
+    {
+        SetActorTickEnabled(false);
+    }
 }
 
