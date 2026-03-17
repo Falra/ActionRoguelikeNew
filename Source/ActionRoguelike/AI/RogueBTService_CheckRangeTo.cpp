675432c2267d39dd217ec20274b66d1e0fe9c30e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2026-03-17 08:47:02 +0200

    Added Service to check range to target

---
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueBTService_CheckRangeTo.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void URogueBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+    UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+    check(BBComp);
+
+    const AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject(TargetActorKey.SelectedKeyName));
+    if (TargetActor)
+    {
+        const AAIController* Controller = OwnerComp.GetAIOwner();
+
+        const APawn* OwningPawn = Controller->GetPawn();
+        check(OwningPawn);
+
+        const float DistanceTo = FVector::Dist(TargetActor->GetActorLocation(), OwningPawn->GetActorLocation());
+
+        const bool bWithinRange = DistanceTo < MaxAttackRange;
+
+        const bool bHasLOS = Controller->LineOfSightTo(TargetActor);
+
+        BBComp->SetValueAsBool(WithinRangeKey.SelectedKeyName, (bWithinRange && bHasLOS));
+    }
+}
