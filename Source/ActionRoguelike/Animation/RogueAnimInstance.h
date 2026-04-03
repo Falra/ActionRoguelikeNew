5f2902be1c6f8f95cbf67a261c6c0ae3d0ad72c0
Valerii Diachenko <leriy.dyachenko@gmail.com>
2026-04-03 10:28:11 +0300

    Added Sprint "TravelMode" Animation

---
+++ Source/ActionRoguelike/Animation/RogueAnimInstance.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "RogueAnimInstance.generated.h"
+
+class URogueActionSystemComponent;
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API URogueAnimInstance : public UAnimInstance
+{
+    GENERATED_BODY()
+
+public:
+    virtual void NativeInitializeAnimation() override;
+
+    virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+
+    UPROPERTY(Transient, BlueprintReadOnly, Category = "StatusEffects")
+    bool bIsSprinting;
+
+    UPROPERTY(Transient, BlueprintReadOnly)
+    TObjectPtr<URogueActionSystemComponent> ActionComp;
+
+};
