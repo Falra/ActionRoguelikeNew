6303534e1f793384c915f259335b944149a397e8
Valerii Diachenko <leriy.dyachenko@gmail.com>
2026-04-09 08:08:42 +0300

    Added Meshes with ISM to the coin pickups

--- Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.cpp
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.cpp
@@ -5,18 +5,32 @@
 
 #include "ActionRoguelike.h"
 #include "EngineUtils.h"
+#include "Components/InstancedStaticMeshComponent.h"
 #include "Player/RoguePlayerCharacter.h"
 
 void URogueCoinPickupSubsystem::AddCoinPickups(const TArray<FVector>& NewLocations, const TArray<int32>& NewAmounts)
 {
     CoinLocations.Append(NewLocations);
     CoinAmounts.Append(NewAmounts);
+    
+    TArray<FTransform> MeshTransforms;
+    for (int i = 0; i < NewLocations.Num(); ++i)
+    {
+        MeshTransforms.Add(FTransform(NewLocations[i] + FVector(0, 0, 50.0f)));
+    }
+
+    TArray<FPrimitiveInstanceId> NewMeshIDs = WorldISM->AddInstancesById(MeshTransforms, true,false);
+
+    MeshIDs.Append(NewMeshIDs);
 }
 
 void URogueCoinPickupSubsystem::RemoveCoinPickup(int32 IndexToRemove)
 {
     CoinLocations.RemoveAt(IndexToRemove);
     CoinAmounts.RemoveAt(IndexToRemove);
+    
+    WorldISM->RemoveInstanceById(MeshIDs[IndexToRemove]);
+    MeshIDs.RemoveAt(IndexToRemove);
 }
 
 void URogueCoinPickupSubsystem::Tick(float DeltaTime)
@@ -61,4 +75,20 @@ void URogueCoinPickupSubsystem::Tick(float DeltaTime)
     {
         DrawDebugPoint(World, CoinLocations[i], 8.0f, FColor::White);
     }
+}
+
+void URogueCoinPickupSubsystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+    Super::OnWorldBeginPlay(InWorld);
+    
+    UWorld* World = GetWorld();
+
+    // Temporary Hack - we will fix with developer settings soon.
+    const FSoftObjectPath MeshAssetPath(TEXT("/Game/ExampleContent/Meshes/SM_Pickup_Coin.SM_Pickup_Coin"));
+    UStaticMesh* LoadedMesh = Cast<UStaticMesh>(MeshAssetPath.TryLoad());
+
+    WorldISM = NewObject<UInstancedStaticMeshComponent>(World, NAME_None, RF_Transient);
+    WorldISM->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    WorldISM->SetStaticMesh(LoadedMesh);
+    WorldISM->RegisterComponentWithWorld(World);
 }
