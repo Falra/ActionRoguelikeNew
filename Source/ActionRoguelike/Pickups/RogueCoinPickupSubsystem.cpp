64678e36ad0b530ac9a4708bdac548cf2d155976
Valerii Diachenko <leriy.dyachenko@gmail.com>
2026-05-19 08:27:49 +0300

    Added cycle counters for insights + fixed audio spatialization bug

--- Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.cpp
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.cpp
@@ -9,9 +9,14 @@
 #include "Components/InstancedStaticMeshComponent.h"
 #include "Core/RogueDeveloperSettings.h"
 #include "Player/RoguePlayerCharacter.h"
+#include "ProfilingDebugging/CountersTrace.h"
+
+TRACE_DECLARE_INT_COUNTER(CoinInstanceCount, TEXT("Coins in World"));
 
 void URogueCoinPickupSubsystem::AddCoinPickups(const TArray<FVector>& NewLocations, const TArray<int32>& NewAmounts)
 {
+    TRACE_CPUPROFILER_EVENT_SCOPE(CoinPickupSubsystem::AddCoinPickups);
+
     CoinLocations.Append(NewLocations);
     CoinAmounts.Append(NewAmounts);
     
@@ -24,20 +29,28 @@ void URogueCoinPickupSubsystem::AddCoinPickups(const TArray<FVector>& NewLocatio
     TArray<FPrimitiveInstanceId> NewMeshIDs = WorldISM->AddInstancesById(MeshTransforms, true,false);
 
     MeshIDs.Append(NewMeshIDs);
+    
+    TRACE_COUNTER_SET(CoinInstanceCount, CoinLocations.Num());
 }
 
 void URogueCoinPickupSubsystem::RemoveCoinPickup(int32 IndexToRemove)
 {
+    TRACE_CPUPROFILER_EVENT_SCOPE(CoinPickupSubsystem::RemoveCoinPickup);
+    
     CoinLocations.RemoveAt(IndexToRemove);
     CoinAmounts.RemoveAt(IndexToRemove);
     
     WorldISM->RemoveInstanceById(MeshIDs[IndexToRemove]);
     MeshIDs.RemoveAt(IndexToRemove);
+    
+    TRACE_COUNTER_SET(CoinInstanceCount, CoinLocations.Num());
 }
 
 void URogueCoinPickupSubsystem::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+    
+    TRACE_CPUPROFILER_EVENT_SCOPE(CoinPickupSubsystem::Tick);
     
     const UWorld* World = GetWorld();
     
@@ -51,30 +64,39 @@ void URogueCoinPickupSubsystem::Tick(float DeltaTime)
 
     TArray<int32> ProcessList;
 	
-    for (int i = 0; i < CoinLocations.Num(); ++i)
+    // Distance Check
     {
-        const float Dist = FVector::Dist(PlayerLocation, CoinLocations[i]);
-        if (Dist < PickupRadius)
+        TRACE_CPUPROFILER_EVENT_SCOPE(CoinPickupSubsystem::Tick::DistanceCheck);
+
+        for (int i = 0; i < CoinLocations.Num(); ++i)
         {
-            ProcessList.Add(i);
+            const float Dist = FVector::Dist(PlayerLocation, CoinLocations[i]);
+            if (Dist < PickupRadius)
+            {
+                ProcessList.Add(i);
+            }
         }
     }
 
     int32 TotalCoinsToGrant = 0;
-    for (int i = ProcessList.Num()-1; i >= 0; --i)
     {
-        int32 CoinIndex = ProcessList[i];
-		
-        TotalCoinsToGrant += CoinAmounts[CoinIndex];
+        TRACE_CPUPROFILER_EVENT_SCOPE(CoinPickupSubsystem::Tick::HandlePickups);
 
-        RemoveCoinPickup(CoinIndex);
+        for (int i = ProcessList.Num() - 1; i >= 0; --i)
+        {
+            const int32 CoinIndex = ProcessList[i];
+
+            TotalCoinsToGrant += CoinAmounts[CoinIndex];
+
+            RemoveCoinPickup(CoinIndex);
+        }
     }
 
     if (TotalCoinsToGrant > 0)
     {
         PlayPickupSound();
     }
-    
+#if 0    
     // @todo: grant coins to player(s)
     UE_CLOG(TotalCoinsToGrant > 0, LogGame, Log, TEXT("Picked up Coin Amount=%d"), TotalCoinsToGrant);
     
@@ -82,6 +104,7 @@ void URogueCoinPickupSubsystem::Tick(float DeltaTime)
     {
         DrawDebugPoint(World, CoinLocations[i], 8.0f, FColor::White);
     }
+#endif   
 }
 
 void URogueCoinPickupSubsystem::OnWorldBeginPlay(UWorld& InWorld)
@@ -94,6 +117,8 @@ void URogueCoinPickupSubsystem::OnWorldBeginPlay(UWorld& InWorld)
     WorldISM->SetCollisionEnabled(ECollisionEnabled::NoCollision);
     WorldISM->RegisterComponentWithWorld(World);
     
+    TRACE_COUNTER_SET(CoinInstanceCount, 0);
+    
     const URogueDeveloperSettings* DevSettings = GetDefault<URogueDeveloperSettings>();
     CoinPickupTriggerParamName = DevSettings->CoinPickupTriggerParameter;
     
@@ -102,7 +127,9 @@ void URogueCoinPickupSubsystem::OnWorldBeginPlay(UWorld& InWorld)
     
     WorldAudioComp = NewObject<UAudioComponent>(World, NAME_None, RF_Transient);
     WorldAudioComp->SetAutoActivate(false);
+    WorldAudioComp->bAllowSpatialization = false;
     WorldAudioComp->RegisterComponentWithWorld(World);
+    
 
     DevSettings->CoinPickupSound.LoadAsync(
         FLoadSoftObjectPathAsyncDelegate::CreateUObject(this, &ThisClass::OnPickupSoundLoadComplete));
